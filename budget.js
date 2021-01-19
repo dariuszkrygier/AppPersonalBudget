65955ceeb852bfcc21701276acc8e44a537fadbb
Dariusz Krygier <dariusz.krygier.programista@gmail.com>
2021-01-19 08:51:08 +0100

    changes

--- budget.js
+++ budget.js
@@ -1,4 +1,24 @@
-document.querySelector("#today").valueAsDate = new Date();
+//$(document).ready(function(){
+//	$('#dropdownMenu2').hide();	
+//	});
+
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = d.getFullYear() + '/' +
+    (month<10 ? '0' : '') + month + '/' +
+    (day<10 ? '0' : '') + day;
+
+
+               
+                document.getElementById('wynik').innerHTML = output;
+      
+
+
+			
+	document.querySelector("#today").valueAsDate = new Date();
 
 	 
         var form = document.querySelector('.needs-validation');
@@ -12,10 +32,7 @@ document.querySelector("#today").valueAsDate = new Date();
         })
 		
 		
-$(document).ready(function(){
-	$('#dropdownMenu2').hide();
-		
-	});
+
     
 
 
